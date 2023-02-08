45a96c02b1695f5accc48c44574eda7ccd4e93dc
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2023-02-08 18:04:59 -0300

    fix: improved tests code

--- test/FamilyFunds.js
+++ test/FamilyFunds.js
@@ -1,28 +1,30 @@
 const { expect } = require("chai");
 
 describe('FamilyFunds', async function () {
-  
   it('should set the right owner', async function () {
-    const [owner, addr1, addr2] = await ethers.getSigners()
+    const [ownerAccount, account1, account2] = await ethers.getSigners()
+    const [ownerAddress, addr1, addr2] = addresses.map(account => account.address)
+
     const FamilyFundsContract = await ethers.getContractFactory('FamilyFunds')
-    const familyFunds = await FamilyFundsContract.deploy([addr1.address, addr2.address])
+    const familyFunds = await FamilyFundsContract.deploy([addr1, addr2])
     
     await familyFunds.deployTransaction.wait()
 
-    expect(await familyFunds.owner()).to.be.equal(owner.address)
+    expect(await familyFunds.owner()).to.be.equal(ownerAddress)
   })
 
   it('should recognise addresses as family members', async function () {
-    const [owner, addr1, addr2, addr3] = await ethers.getSigners()
+    const [ownerAcc, account1, account2, account3] = await ethers.getSigners()
+    const [ownerAddress, addr1, addr2, addr3] = addresses.map(account => account.address)
+
     const FamilyFundsContract = await ethers.getContractFactory('FamilyFunds')
-    const familyFunds = await FamilyFundsContract.deploy([addr1.address, addr2.address])
+    const familyFunds = await FamilyFundsContract.deploy([addr1, addr2])
 
     await familyFunds.deployTransaction.wait()
 
-    expect(await familyFunds.isFamilyMember(addr1.address)).to.equal(true)
-    expect(await familyFunds.isFamilyMember(addr2.address)).to.equal(true)
-    expect(await familyFunds.isFamilyMember(addr3.address)).to.equal(false)
-
+    expect(await familyFunds.isFamilyMember(addr1)).to.equal(true)
+    expect(await familyFunds.isFamilyMember(addr2)).to.equal(true)
+    expect(await familyFunds.isFamilyMember(addr3)).to.equal(false)
   })
 })
 
